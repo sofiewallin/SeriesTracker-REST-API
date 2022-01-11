97d8e18ad39a6c373c458d001624bfa0e9a714bb
Sofie Wallin <sofie.wallin84@gmail.com>
2022-01-11 08:50:41 +0100

    Fix headers with cors

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.use(express.json());
 // Mongoose
 const mongoose = require('mongoose');
 
+// Cors 
+const cors = require('cors');
+
+app.use(cors());
+
 /**
  * 1. Connect to database
  */
@@ -28,17 +33,6 @@ mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to database'));
-
-/**
- * 2. Set API headers
- */
-
-app.all('/api/*', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-	next();
-});
 
 /**
  * 3. Set API routes
